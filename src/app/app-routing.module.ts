ac2e6daf88106ecba097dda08c1bf842191f4cb1
Francisco Durante <fdurante@alu.ucam.edu>
2022-06-10 12:31:51 +0200

    modulos y componentes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes: Routes = [
+  {
+    // Cuando alguien entre al 'auth' se cargarán sus módulos hijos
+    // devolviendo el módulo 'AuthModule'
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    //component: ErrorPageComponent
+    redirectTo: '404'
+  }
+]
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
