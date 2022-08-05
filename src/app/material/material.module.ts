e180f07cf5ec29ffb7dea45747272b39eeb72084
Francisco Durante <fdurante@alu.ucam.edu>
2022-08-05 14:10:43 +0200

    lección 199

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   exports: [
+    MatAutocompleteModule,
     MatButtonModule,
-    MatIconModule,
-    MatListModule,
-    MatSidenavModule,
     MatCardModule,
-    MatToolbarModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
     MatProgressSpinnerModule,
-    MatGridListModule
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
